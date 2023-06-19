abe8de6c4882985c4953d5421ea099e26056ea72
Olala <olasabbouh@gmail.com>
2023-06-19 10:13:22 +0300

    12

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,20 @@
-import express, {Request, Response} from "express";
+import express from "express";
+import {getAll, getOneById, create, updateById, deleteById} from '../src/controllers/planets'
 const app = express();
 const port = 3000;
 
 
 app.use(express.json());
 
-type Planet = {
-    id: number,
-    name: string,
-  };
+app.get("/getPlanets", getAll)
 
-type Planets = Planet[];
+app.get("/getPlanets/:id", getOneById)
 
-let planets: Planets = [
-    {
-      id: 1,
-      name: "Earth",
-    },
-    {
-      id: 2,
-      name: "Mars",
-    },
-  ];
+app.post("/addPlanet", create)
 
-app.get("/getPlanets", (req: Request, res: Response) => {
-  res.status(200).json(planets)
-})
+app.put("/editPlanet/:id", updateById)
 
-app.get("/getPlanets/:id", (req: Request, res: Response) => {
-  const {id} = req.params
-  const planet = planets.find(p => p.id === Number(id))
-  res.status(200).json(planet)
-})
-
-app.post("/addPlanet", (req: Request, res: Response) => {
-  const {id, name} = req.body;
-  const newPlanet = {id, name};
-  planets = [...planets, newPlanet];
-  console.log(planets)
-  res.status(201).json({msg: 'the planet is created'})
-})
-
-app.put("/editPlanet/:id", (req: Request, res: Response) => {
-  const {id} = req.params;
-  const {name} = req.body;
-  planets = planets.map((p) => (p.id === Number(id) ? {...p, name} : p))
-  console.log(planets)
-  res.status(201).json({msg: 'the planet is edited'})
-})
-
-app.delete("/deletePlanet/:id", (req: Request, res: Response) => {
-  const {id} = req.params;
-  planets = planets.filter((p) => (p.id !== Number(id)))
-  
-  res.status(200).json({msg: 'the planet is deleted'})
-})
+app.delete("/deletePlanet/:id", deleteById)
 
 
 app.listen(port, () => {
